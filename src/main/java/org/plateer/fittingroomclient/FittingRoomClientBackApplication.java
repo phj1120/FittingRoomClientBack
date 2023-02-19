397e0d28ca081d4d3277bdb196fd2c56f5bc947c
phj1120 <hyeonj1998@naver.com>
2023-02-19 13:20:02 +0900

    init: project

---
+++ src/main/java/org/plateer/fittingroomclient/FittingRoomClientBackApplication.java
@@ -0,0 +1,13 @@
+package org.plateer.fittingroomclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FittingRoomClientBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FittingRoomClientBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/plateer/fittingroomclient/FittingRoomClientBackApplicationTests.java
@@ -0,0 +1,13 @@
+package org.plateer.fittingroomclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FittingRoomClientBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
