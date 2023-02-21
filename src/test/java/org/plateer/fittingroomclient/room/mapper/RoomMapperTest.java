b4a457d0c2d3d1bf58d6d677f0420c6e26a4b022
Seunghyeon Jung <armyso9506@plateer.com>
2023-02-21 17:56:23 +0900

    feat( XML ) : XML 수정

--- src/test/java/org/plateer/fittingroomclient/room/mapper/RoomMapperTest.java
+++ src/test/java/org/plateer/fittingroomclient/room/mapper/RoomMapperTest.java
@@ -3,7 +3,9 @@ package org.plateer.fittingroomclient.room.mapper;
 
 import lombok.extern.log4j.Log4j2;
 import org.junit.jupiter.api.Test;
+import org.plateer.fittingroomclient.common.dto.PageResultDTO;
 import org.plateer.fittingroomclient.room.dto.RoomDTO;
+import org.plateer.fittingroomclient.room.dto.RoomPageRequestDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -17,7 +19,21 @@ public class RoomMapperTest {
 
     @Test
     void getRoomList() {
-        List<RoomDTO> dtoList = roomMapper.getRoomList();
+        RoomPageRequestDTO roomPageRequestDTO = new RoomPageRequestDTO(1, 10);
+        List<RoomDTO> dtoList = roomMapper.getRoomList(roomPageRequestDTO);
+        int total = roomMapper.getRoomCount(roomPageRequestDTO);
+
+        PageResultDTO<RoomDTO> list = PageResultDTO.<RoomDTO>withAll().dtoList(dtoList).total(total).pageRequestDTO(roomPageRequestDTO).build();
+        log.info(list);
+    }
+
+    @Test
+    void getRoomSellerList() {
+        RoomDTO roomDTO = RoomDTO.builder()
+                .pmNo(1L)
+                .build();
+
+        List<RoomDTO> dtoList = roomMapper.getRoomSellerList(roomDTO);
         log.info(dtoList);
     }
 }
